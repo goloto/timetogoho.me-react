a33c6c2da6ec974a2f9e9fc594d9fb8cc70ee1ab
Mikhail Goloto <goloto@pm.me>
2021-06-11 11:56:08 +0300

    add Timer component

--- src/Components/Timer/Timer.tsx
+++ src/Components/Timer/Timer.tsx
@@ -4,7 +4,7 @@ import { Hours } from '../Hours/Hours';
 import { Minutes } from "../Minutes";
 import { Seconds } from "../Seconds/Seconds";
 
-function Timer() {
+export function Timer() {
   const smallWindow = window.matchMedia('max-aspect-ratio: 9 / 16').matches;
   const mediumWindow = window.matchMedia('max-aspect-ratio: 8 / 7').matches;
   const bigWindow = !mediumWindow && !smallWindow;
