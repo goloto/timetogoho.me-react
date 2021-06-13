969d81b8c2e4733f1acd2134c9682c3a4c854a38
Mikhail Goloto <goloto@pm.me>
2021-06-13 16:24:13 +0300

    add isWorkingDay text differences

--- src/Components/Timer/Timer.tsx
+++ src/Components/Timer/Timer.tsx
@@ -8,17 +8,18 @@ export function Timer() {
   const smallWindow = window.matchMedia('max-aspect-ratio: 9 / 16').matches;
   const mediumWindow = window.matchMedia('max-aspect-ratio: 8 / 7').matches;
   const bigWindow = !mediumWindow && !smallWindow;
+  const isWorkingDay = true;
 
   return(
     <div className='timer'>
-      {mediumWindow && <TimerDescription text={'time to go home'} />}
+      {mediumWindow && <TimerDescription text={isWorkingDay ? 'time to go home' : 'time until working day'} />}
       {(bigWindow || smallWindow) && <TimerDescription text={'time'} />}
       <Hours />
-      {(bigWindow || smallWindow) && <TimerDescription text={'to'} />}
+      {(bigWindow || smallWindow) && <TimerDescription text={isWorkingDay ? 'to' : 'until'} />}
       <Minutes />
-      {(bigWindow || smallWindow) && <TimerDescription text={'go'} />}
+      {(bigWindow || smallWindow) && <TimerDescription text={isWorkingDay ? 'go' : 'working'} />}
       <Seconds />
-      {(bigWindow || smallWindow) &&  <TimerDescription text={'home'} />}
+      {(bigWindow || smallWindow) &&  <TimerDescription text={isWorkingDay ? 'home' : 'day'} />}
     </div>
   );
 }
