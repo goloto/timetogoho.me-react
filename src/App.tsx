c06dfc0657aac5c2f378dae43402c7d745ef1dbd
Mikhail Goloto <goloto@pm.me>
2021-06-22 12:10:10 +0300

    add AppCanvas styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Timer } from "./Components/Timer/Timer";
 import './App.css';
+import styled from "styled-components";
 
 function App() {
+  const AppCanvas = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100vw;
+    height: 100vh;
+  `;
+
   return (
-    <Timer/>
+    <AppCanvas>
+      <Timer/>
+    </AppCanvas>
   );
 }
 
