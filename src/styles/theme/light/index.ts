89e92ea8cec34b11385f73dff9d0efdbc634c47e
Mikhail Goloto <goloto@pm.me>
2021-06-28 17:54:14 +0300

    add Light theme values

---
+++ src/styles/theme/light/index.ts
@@ -0,0 +1,17 @@
+import {ITheme, IThemeColors} from "../theme.type";
+
+const lightColors: IThemeColors = {
+  background: `rgb(240, 240, 250)`,
+  textContrast: `rgb(55, 55, 75)`,
+  textSoft: `rgb(200, 200, 230)`,
+  buttonBackgroundGray: `rgb(200, 200, 230)`,
+  buttonBackgroundGrayHover: `rgb(225, 225, 255)`,
+  buttonBackgroundBlue: `rgb(200, 200, 255)`,
+  buttonBackgroundBlueHover: `rgb(220, 220, 255)`,
+  buttonBackgroundViolet: `rgb(170, 170, 240)`,
+  buttonBackgroundVioletHover: `rgb(205, 205, 255)`,
+}
+
+export const light: ITheme = {
+  colors: lightColors
+}
