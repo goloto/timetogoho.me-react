2bfb48c40c5ab60c36ce9a7243f1f5755af4f9d7
Mikhail Goloto <goloto@pm.me>
2021-06-28 17:53:20 +0300

    add TimerDescription styles

--- src/core/components/TimerDescription/TimerDescription.component.tsx
+++ src/core/components/TimerDescription/TimerDescription.component.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import styled from "styled-components";
 
 export function TimerDescription(props: {children: string}) {
+  const Container = styled.div`
+    display: flex;
+    justify-content: center;
+  `;
+  const Text = styled.p`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: var(--soft-color_2);
+    font-size: var(--third-font-size);
+    line-height: var(--third-font-size);
+    text-transform: uppercase;
+  `;
+
   return(
-    <div className="text-group text-group_separated">
-      <div className="text">{props.children}</div>
-    </div>
+    <Container>
+      <Text>
+        {props.children}
+      </Text>
+    </Container>
   );
 }
