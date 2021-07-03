a63bdaaef00448e27e212afb03df4cbe3a081085
Mikhail Goloto <goloto@pm.me>
2021-07-03 20:48:10 +0300

    add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './core/components/App/App.component';
+import { Router } from "react-router";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
