93b23045b4fa09834f944e0c689a02f700714e06
Mikhail Goloto <goloto@pm.me>
2021-08-01 20:53:07 +0300

    delete Time styled component

--- src/core/styled/time/time.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import {fontSizes} from "../../../styles/theme/constants/fontSizes.constant";
-import {useThemeColors} from "../../hooks/useThemeColors.hook";
-
-export const Time = styled.div`
-  font-size: ${fontSizes.lg};
-  line-height: ${fontSizes.lg};
-  color: ${useThemeColors().textContrast};
-`;
